898f730cc4be0d8ca22770d751bec36914e99100
Mariola Pasko <paskomariola@gmail.com>
2020-11-11 22:35:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(i => i * -1);
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(i => i);
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(i => i * 2);
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(i => i*i)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    sourceArray.reduce + startingPoint;
+}
